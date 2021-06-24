65289ba6d051429257c4a19e0210fab1a23e2527
ENMANUEL URENA VARGAS <1095352@est.intec.edu.do>
2021-06-24 17:21:54 -0400

    Anagram_v1.5

---
+++ AnagramConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Anagrams;
+
+namespace AnagramConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("pedro");
+            list.Add("miro");
+            list.Add("drope");
+            list.Add("cora");
+            list.Add("ropde");
+            list.Add("porra");
+            list.Add("romi");
+            var result = Anagram.findAnagrams(list);
+            foreach (var item in result)
+            {
+                System.Console.WriteLine(item);
+            }
+
+        }
+    }
+}
