8ef5d3568d5e2997f250a1813a74e901d86e692f
Enmanuel Urena <enmanuelurenav@gmail.com>
2021-06-24 00:04:39 -0400

    Anagrams_v1

---
+++ Anagrams/Anagrams.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Anagrams
+{
+    public class AnagramCls
+    {
+
+
+        public static bool CheckAnagrams(string word1, string word2)
+        {
+            if (word1 == null || word2 == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (word1.Length != word2.Length)
+            {
+                return false;
+            }
+
+            else
+            {
+                word1 = Reverse(word1);
+                word2 = Reverse(word2);
+                if (word1 == word2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        private static string Reverse(string input)
+        {
+            char[] temp = input.ToLowerInvariant().ToCharArray();
+            Array.Sort(temp);
+            return new string(temp);            
+        }
+    }
+}
